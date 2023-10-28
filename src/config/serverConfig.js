67c205fc95df1d3a508f7babff1aa16ec29484a1
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-10-28 15:04:48 +0530

    Setup basic dotenv

---
+++ src/config/serverConfig.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+// from this file we will be accessing our port
+// url : https://www.npmjs.com/package/dotenv
+// simliar hai require('dotenv').config() . humlog bas dotenv name ka variable bana liye hai phir uske baad .config() function attach kiye h 
+
+
+module.exports = {
+    PORT : process.env.PORT
+}
